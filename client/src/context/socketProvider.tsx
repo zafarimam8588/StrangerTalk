ffda546b3514d04bfd3313da76df0e86d9177bb4
Zafar Imam <zimam8588@gmail.com>
2025-01-17 18:14:55 +0530

    fixed socket bug

--- client/src/context/socketProvider.tsx
+++ client/src/context/socketProvider.tsx
@@ -1,15 +1,15 @@
 import { createContext, useContext, useMemo } from "react";
 import { Socket, io } from "socket.io-client";
 
-const SocketContext = createContext<Socket | null>(null);
+const SocketContext = createContext<Socket | null | string>(null);
 
 export const useSocket = () => {
-  const socket: Socket | null = useContext(SocketContext);
+  const socket: Socket | null | string = useContext(SocketContext);
   return socket;
 };
 
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
-  const socket = useMemo(() => io("http://localhost:5000"), []);
+  const socket = "abc";
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
